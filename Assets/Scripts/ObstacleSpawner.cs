ae2ad59d3ed52a79f19aa5ead5234481f8790743
helenecousin <helenecousin0912@gmail.com>
2026-07-05 22:50:16 +0800

    Multiple adjustments
    I have duplicated my original "Spawner" script, renamed the first to Obstacle Spawner and renamed the second to Platform Spawner.
    I adjusted the size of my spike cluster obstacle as it was too large to jump over.

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour
+public class ObstacleSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] obstaclePrefabs;
     public float obstacleSpawnTime = 6f;
