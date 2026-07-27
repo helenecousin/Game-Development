003162b5882afc4c607eadd16e8e54ec277d7774
helenecousin <helenecousin0912@gmail.com>
2026-07-27 11:55:33 +0800

    Platform Manager
    Edited my existing platform manager script to manage the spawning of my new platform set prefabs.

--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
 
-public class PlatformManager : MonoBehaviour 
+public class PlatformManager : MonoBehaviour
 {
-    public Transform prefab;
+    [SerializeField] private GameObject[] platformPrefabs;
+    [SerializeField] private Transform spawnPoint;
+
+    public void SpawnPlatform()
+    {
+        int randomIndex = Random.Range(0, platformPrefabs.Length);
+
+        Instantiate(platformPrefabs[randomIndex], spawnPoint.position, Quaternion.identity);
+    }
 }
