a0c61d793e79869f782710accc21b998e51b17c5
helenecousin <helenecousin0912@gmail.com>
2026-07-27 13:04:58 +0800

    Platform Spawn Mechanic Adjustments
    I made some adjustments to the platform spawning mechanism.

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -2,10 +2,35 @@ using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private float moveSpeed = 6f;
+    [SerializeField] private float spawnX = -10f;
+    [SerializeField] private float destroyX = -20f;
+
+    private bool hasSpawnedNext = false;
+
+    private PlatformManager platformManager;
+
+    private void Start()
+    {
+        platformManager = FindAnyObjectByType<PlatformManager>();
+    }
 
     private void Update()
     {
+        // Moves the platform left
         transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+
+        // Spawns the next platform
+        if (!hasSpawnedNext && transform.position.x <= spawnX)
+        {
+            platformManager.SpawnPlatform();
+            hasSpawnedNext = true;
+        }
+
+        // Destroys this platform when it is off-screen
+        if (transform.position.x <= destroyX)
+        {
+            Destroy(gameObject);
+        }
     }
 }
