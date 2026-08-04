1ac74692c924b94eb2d1c41c53c54bcbdb9bb927
helenecousin <helenecousin0912@gmail.com>
2026-08-04 17:45:34 +0800

    Changed Score Increment
    Instead of only incrementing the score by 1 for every fruit collectible, which makes progression feel extremely slow, I made the score increment by a random number between 1 and 10.

--- Assets/Scripts/PlayerCollection.cs
+++ Assets/Scripts/PlayerCollection.cs
@@ -6,9 +6,16 @@ public class PlayerCollection : MonoBehaviour
 
     public void CollectIngredient()
     {
-        score ++ ;
+
+        //defining the range limits
+        int minRange = 1;
+        int maxRange = 11;
+
+        int randomIncrement = UnityEngine.Random.Range(minRange, maxRange);
+        score += randomIncrement ;
+
         //later on add broom mechanic here: potionAmount++; if (potionAmount >= potionMax){ActivateBroom();}
 
-        Debug.Log("Score: " + score);
+        Debug.Log("Score: " + score + "(+ " + randomIncrement + ")");
     }
 }
